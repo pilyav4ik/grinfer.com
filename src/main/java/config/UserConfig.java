b60cecc18f827e9ead0eab011b32ba821b4e1b1e
Igor <ipliavskyi@gmail.com>
2020-10-23 19:48:58 +0200

    #1 Create test case login to site and comparison First name and Last name on Settings page

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -3,4 +3,6 @@ package config;
 public class UserConfig {
     public static final String USER_EMAIL = "assassin.gob@gmail.com";
     public static final String USER_PASSWORD = "Password!1";
+    public static final String USER_FIRST_NAME = "Maximus";
+    public static final String USER_LAST_NAME = "Billisimus";
 }
--- src/test/java/steps/SignInPageDef.java
+++ src/test/java/steps/SignInPageDef.java
@@ -2,6 +2,8 @@ package steps;
 
 import config.UserConfig;
 import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import pages.SignInPage;
 
 public class SignInPageDef {
 
@@ -16,5 +18,24 @@ public class SignInPageDef {
     @Then("Input password")
     public void inputPassword(){
         signInPage.setPasswordInput(UserConfig.USER_PASSWORD);
+    }
+
+    @Then("Click {string} div")
+    public void clickDiv(String arg0) {
+        signInPage.clickUserButton();
+    }
+
+    @Then("Check First Name")
+    public void checkFirstName() {
+        String expectValue = UserConfig.USER_FIRST_NAME;
+        String actual = signInPage.getFirstName();
+        Assert.assertEquals(expectValue, actual);
+    }
+
+    @Then("Check Last Name")
+    public void checkLastName() {
+        String expectValue = UserConfig.USER_LAST_NAME;
+        String actual = signInPage.getLastName();
+        Assert.assertEquals(expectValue, actual);
     }
 }
